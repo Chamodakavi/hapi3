92406b56ba7eb955ad88e5834aca3cafec27efcf
Chamoda Kavishka <142435114+Chamodakavi@users.noreply.github.com>
2026-01-18 18:23:34 +0530

    fix:msg changed

--- src/components/businessRegistration/Form.tsx
+++ src/components/businessRegistration/Form.tsx
@@ -237,7 +237,7 @@ function Form() {
                   </svg>
                 </div>
                 <h3 className="text-2xl font-bold text-white mb-2">
-                  Payment successful
+                  Successfully Registered
                 </h3>
                 <p className="text-slate-400 text-sm mb-6">{modalMessage}</p>
                 <button
